3e1f08ed37d7d2aa179f3775f2a5b422ec79406c
avsbestua <rmetrikovskij@gmail.com>
2025-05-06 23:15:20 +0300

    1

--- gui.py
+++ gui.py
@@ -5,6 +5,8 @@ from tkinter.messagebox import *
 from PIL import Image
 from tkinter import colorchooser
 
+from matplotlib.testing import set_font_settings_for_testing
+
 font = ("Segoe UI", 15)
 
 
@@ -17,10 +19,13 @@ def choose_color():
         return (255, 255, 255) #If user won`t select color, white will be used
 
 
+
+
+
 class App:
         def __init__(self):
                 root = tk.Tk()
-                root.geometry("800x550+400+150") #Setting up window
+                root.geometry("800x520+400+150") #Setting up window
                 root['bg'] = 'snow'
                 root.title("EasyQR")
 
@@ -59,6 +64,8 @@ class App:
                                       )
                 eror_cor['values'] = ("7%", "15%", "25%", "30%")
                 eror_cor.place(x=70, y=250)
+                eror_cor.set("7%")
+
 
                 m_btn = tk.Button(root,
                         text="Make QR-Code",
@@ -70,6 +77,7 @@ class App:
                 m_btn.place(x=350,
                             y=410)
 
+
                 self.box_size = tk.Scale(root,
                                          from_= 1,
                                          to=21,
@@ -79,6 +87,9 @@ class App:
                                          length=280)
                 self.box_size.place(x=50,
                                     y=300)
+
+                self.box_size.set(10)
+
 
                 tk.Label(root,
                          text='Select box size',
@@ -96,6 +107,8 @@ class App:
                 self.border_size.place(x=50,
                                     y=400)
 
+                self.border_size.set(5)
+
                 tk.Label(root,
                          text='Select border size',
                          bg='snow',
@@ -112,10 +125,10 @@ class App:
                 image = qr.make_image(fill_color='black', back_color=choose_color())
                 image.save('qr.png')
 
+                img = Image.open("qr.png")
+                img.show()
 
             else: showerror("Error", "Please enter information to code")
-
-
 
         def generate(self):     #Qr code generator
             print("work")
@@ -130,8 +143,6 @@ class App:
                 elif eror_corect == '30%':
                     self.qr_code(qrcode.ERROR_CORRECT_H)
 
-                img = Image.open("qr.png")
-                img.show()
 
             else: showerror("Error", "Select error correction level")
 
